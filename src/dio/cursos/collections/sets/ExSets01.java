376bb3af3fbd8852856bafd5be0b8f5a5dc9304b
joaohab <joao_hab@hotmail.com>
2022-11-25 11:41:30 -0300

    Collections
    Conclusão das atividades de Sets.

--- src/dio/cursos/collections/sets/ExSets01.java
+++ src/dio/cursos/collections/sets/ExSets01.java
@@ -1,5 +1,9 @@
 package dio.cursos.collections.sets;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 /*
  * Crie um conjunto contendo as cores do arco-íris e:
  * 
@@ -13,6 +17,81 @@ package dio.cursos.collections.sets;
  * 8 - Confira se o conjunto está vazio
  */
 
+ // vermelha, laranja, amarela, verde, azul, azul-escuro e violeta
+
 public class ExSets01 {
     
+    public static void main(String[] args) {
+        
+        // Criando o conjunto e inserindo os elementos
+        Set<String> arcoIris = new HashSet<>();
+        arcoIris.add("vermelha");
+        arcoIris.add("laranja");
+        arcoIris.add("amarela");
+        arcoIris.add("verde");
+        arcoIris.add("azul");
+        arcoIris.add("azul-escuro");
+        arcoIris.add("violeta");
+
+
+        // 1 - Exiba todas as cores uma abaixo da outra
+        System.out.println("\n1 - Exiba todas as cores uma abaixo da outra: ");
+        for (String cor : arcoIris) System.out.println(cor);
+
+
+        // 2 - A quantidade de cores que o arco-íris tem
+        System.out.println("\n2 - A quantidade de cores que o arco-íris tem: ");
+        System.out.println(arcoIris.size());
+
+
+        // 3 - Exiba as cores em ordem alfabética
+        System.out.println("\n3 - Exiba as cores em ordem alfabética: ");
+        Set<String> arcoIrisTree = new TreeSet<>();
+        arcoIrisTree.addAll(arcoIris);
+        for (String cor : arcoIrisTree) System.out.println(cor);
+
+
+        // 4 - Exiba as cores na ordem inversa da que foi informada
+        System.out.println("\n4 - Exiba as cores na ordem inversa da que foi informada: ");
+        Set<String> arcoIrisInverso = ((TreeSet<String>) arcoIrisTree).descendingSet();
+        for (String cor : arcoIrisInverso) System.out.println(cor);
+
+
+        // 5 - Exiba todas as cores que começam com a letra 'v'
+        System.out.println("\n5 - Exiba todas as cores que começam com a letra 'v': ");
+        for (String cor : arcoIrisTree) {
+            char primeiraLetra = cor.charAt(0);
+            if (primeiraLetra == 'v') System.out.println(cor);
+        }
+
+
+        // 6 - Remova todas as cores que não começam com a letra 'v'
+        System.out.println("\n6 - Remova todas as cores que não começam com a letra 'v': ");
+        Set<String> arcoIrisDel = new TreeSet<>();
+        arcoIrisDel.addAll(arcoIris);
+        System.out.println(arcoIrisDel);
+        for (String cor : arcoIrisTree) {
+            char primeiraLetra = cor.charAt(0);
+            if (primeiraLetra != 'v') arcoIrisDel.remove(cor);
+        }
+        System.out.println(arcoIrisDel);
+
+
+        // 7 - Limpe o conjunto
+        System.out.println("\n7 - Limpe o conjunto ");
+        arcoIrisDel.clear();
+        System.out.println(arcoIrisDel);
+
+
+        // 8 - Confira se o conjunto está vazio
+        System.out.println("\n8 - Confira se o conjunto está vazio: ");
+        System.out.println("Conjunto 'arcoIrisDel' esta vazio? \t" + arcoIrisDel.isEmpty());
+        System.out.println("Conjunto 'arcoIris' esta vazio? \t" + arcoIris.isEmpty());
+        System.out.println("Conjunto 'arcoIrisTree' esta vazio? \t" + arcoIrisTree.isEmpty());
+        System.out.println("Conjunto 'arcoIrisInverso' esta vazio? \t" + arcoIrisInverso.isEmpty());
+
+
+
+    }
+
 }
--- src/dio/cursos/collections/sets/ExSets2.java
+++ src/dio/cursos/collections/sets/ExSets2.java
@@ -1,5 +1,10 @@
 package dio.cursos.collections.sets;
 
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 /*
  * Crie uma classe LinguagemFavorita que possua os atributos nome, anoDeCriacao e ide. 
  * Em seguida, crie um conjunto com 3 linguagens e faça um programa que ordene esse conjunto por:
@@ -14,5 +19,136 @@ package dio.cursos.collections.sets;
  */
 
 public class ExSets2 {
+
+    public static void main(String[] args) {
+
+        Set<LinguagemFavorita> linguagem = new LinkedHashSet<>();
+        linguagem.add(new LinguagemFavorita("Java", 1991, "IntelliJ"));
+        linguagem.add(new LinguagemFavorita("Game Maker Language", 1999, "GameMaker Studio"));
+        linguagem.add(new LinguagemFavorita("JavaScript", 1995, "Visual Studio Code"));
+        linguagem.add(new LinguagemFavorita("Python", 1991, "PyCharm"));
+        linguagem.add(new LinguagemFavorita("C#", 1999, "Visual Studio"));
+
+
+        // 1 - Ordem de Inserção
+        System.out.println("\n1 - Ordem de Inserção: ");
+        for (LinguagemFavorita linha : linguagem) System.out.println(linha);
+
+
+        // 2 - Ordem Natural (nome)
+        System.out.println("\n2 - Ordem Natural (nome): ");
+        Set<LinguagemFavorita> linguagemNome = new TreeSet<>(new compareNome());
+        linguagemNome.addAll(linguagem);
+        for (LinguagemFavorita linha : linguagemNome) System.out.println(linha);
+
+
+        // 3 - IDE
+        System.out.println("\n3 - IDE: ");
+        Set<LinguagemFavorita> linguagemIde = new TreeSet<>(new compareIde());
+        linguagemIde.addAll(linguagem);
+        for (LinguagemFavorita linha : linguagemIde) System.out.println(linha);
+
+
+        // 4 - Ano de criação e nome
+        System.out.println("\n4 - Ano de criação e nome: ");
+        Set<LinguagemFavorita> linguagemAnoNome = new TreeSet<>(new compareAnoNome());
+        linguagemAnoNome.addAll(linguagem);
+        for (LinguagemFavorita linha : linguagemAnoNome) System.out.println(linha);
+
+
+        // 5 - Nome, ano de criacao e IDE
+        System.out.println("\n5 - Nome, ano de criacao e IDE: ");
+        Set<LinguagemFavorita> linguagemNomeAnoIde = new TreeSet<>(new compareNomeAnoIde());
+        linguagemNomeAnoIde.addAll(linguagem);
+        for (LinguagemFavorita linha : linguagemNomeAnoIde) System.out.println(linha);
+        
+    }
     
+}
+
+
+
+class LinguagemFavorita {
+
+    private String nome;
+    private int anoDeCriacao;
+    private String ide;
+
+
+    public LinguagemFavorita(String nome, int anoDeCriacao, String ide) {
+        this.nome = nome;
+        this.anoDeCriacao = anoDeCriacao;
+        this.ide = ide;
+
+
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getAnoDeCriacao() {
+        return anoDeCriacao;
+    }
+    public String getIde() {
+        return ide;
+    }
+
+
+    @Override
+    public String toString() {
+        return "[Nome=" + nome + ", Ano De Criação=" + anoDeCriacao + ", IDE=" + ide + "]";
+    }
+
+}
+
+
+
+class compareNome implements Comparator<LinguagemFavorita> {
+
+    @Override
+    public int compare(LinguagemFavorita o1, LinguagemFavorita o2) {
+        return o1.getNome().compareTo(o2.getNome());
+    }
+
+}
+
+
+
+class compareIde implements Comparator<LinguagemFavorita> {
+
+    @Override
+    public int compare(LinguagemFavorita o1, LinguagemFavorita o2) {
+        return o1.getIde().compareTo(o2.getIde());
+    }
+
+}
+
+
+
+class compareAnoNome implements Comparator<LinguagemFavorita> {
+
+    @Override
+    public int compare(LinguagemFavorita o1, LinguagemFavorita o2) {
+        int ano = Integer.compare(o1.getAnoDeCriacao(), o2.getAnoDeCriacao());
+        if (ano != 0) return ano;
+        
+        return o1.getNome().compareTo(o2.getNome());
+    }
+
+}
+
+
+
+class compareNomeAnoIde implements Comparator<LinguagemFavorita> {
+
+    @Override
+    public int compare(LinguagemFavorita o1, LinguagemFavorita o2) {
+        int nome = o1.getNome().compareTo(o2.getNome());
+        if (nome != 0) return nome;
+
+        int ano = Integer.compare(o1.getAnoDeCriacao(), o2.getAnoDeCriacao());
+        if (ano != 0) return ano;
+
+        return o1.getIde().compareTo(o2.getIde());
+    }
+
 }
