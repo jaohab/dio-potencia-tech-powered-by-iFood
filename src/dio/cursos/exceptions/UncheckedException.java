db8cb30c981f8095944671c6cee3a7975ea97cdb
joaohab <joao_hab@hotmail.com>
2022-12-14 00:19:17 -0300

    Update UncheckedException.java

--- src/dio/cursos/exceptions/UncheckedException.java
+++ src/dio/cursos/exceptions/UncheckedException.java
@@ -5,11 +5,25 @@ import javax.swing.*;
 public class UncheckedException {
 
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Numerador: ");
-        String b = JOptionPane.showInputDialog("Denominador: ");
-
-        int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-        System.out.println("Resultado: " + resultado);
+        boolean continuar = true;
+        do {
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+            try {
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                continuar = false;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Entrada inválida, informe um número inteiro!\n" + e.getMessage());
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Entrada inválida, impossível dividir por 0!\n" + e.getMessage());
+            } finally {
+                System.out.println("Chegou no finally.");
+            }
+        } while (continuar);
+        System.out.println("o código continua...");
     }
 
     public static int dividir(int a, int b) {
